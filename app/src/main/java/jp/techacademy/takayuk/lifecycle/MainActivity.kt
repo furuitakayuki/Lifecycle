a655bd43563e19472ac48e5984896139680a5302
furuitakayuki <maki1904@icloud.com>
2022-09-24 08:53:52 +0900

    ライフサイクルメソッド

--- app/src/main/java/jp/techacademy/takayuk/lifecycle/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/lifecycle/MainActivity.kt
@@ -2,10 +2,43 @@ package jp.techacademy.takayuk.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Android", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Android", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Android", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Android", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Android", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Android", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Android", "onRestart")
     }
 }
